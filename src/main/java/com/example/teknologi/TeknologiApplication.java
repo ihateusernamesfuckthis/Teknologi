73e27e366b8a67ee9369c5b8431391451e09383e
ihateusernamesfuckthis <pepe0003@kea.stud.dk>
2025-06-10 22:50:03 +0200

    første commit - teknologi eksamen HTTPS konfig

---
+++ src/main/java/com/example/teknologi/TeknologiApplication.java
@@ -0,0 +1,20 @@
+package com.example.teknologi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class TeknologiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeknologiApplication.class, args);
+    }
+
+    @GetMapping("/")
+    public String hello(){
+        return "HTTPS virker";
+    }
+}
---
+++ src/test/java/com/example/teknologi/TeknologiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.teknologi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeknologiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
